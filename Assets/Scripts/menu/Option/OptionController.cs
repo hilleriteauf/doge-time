033637dd7d266433219a2c2d2753f5981f5d9a04
Titouan Le Floch <titouan.lelfoch@orange.fr>
2022-07-06 21:40:55 +0200

    Amélioration trigger + ajout valeur vitesse

--- Assets/Scripts/menu/Option/OptionController.cs
+++ Assets/Scripts/menu/Option/OptionController.cs
@@ -6,6 +6,13 @@ public class OptionController : MonoBehaviour
     public GameObject scrollbarVitesse;
     public GameObject titreBackMenu;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sound.Initialisation(scrollbarVolume);
+        Vitesse.Initialisation(scrollbarVitesse);
+    }
+
     // Update is called once per frame
     void Update()
     {
